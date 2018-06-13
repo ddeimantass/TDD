b6494e2f668d55b23ae487e6e1363fa97bdee15b
Deimantas Abelskis <ddeimantass@gmail.com>
2018-06-13 19:41:53 +0300

    green

--- src/Model/Parking.php
+++ src/Model/Parking.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: DeimantasAbelskis
- * Date: 2018-06-13
- * Time: 19:39
- */
 
 namespace Testing\Model;
 
--- tests/ParkingTest.php
+++ tests/ParkingTest.php
@@ -3,11 +3,13 @@
 namespace Testing\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Testing\Model\Parking;
 
 class ParkingTest extends TestCase
 {
     public function testParking()
     {
         $parking = new Parking();
+        $this->assertInstanceOf(Parking::class, $parking);
     }
 }
