2495c1dc5d9697dc0421df317346e71dc2a92230
Deimantas Abelskis <ddeimantass@gmail.com>
2018-06-13 19:45:31 +0300

    err2

--- tests/ParkingTest.php
+++ tests/ParkingTest.php
@@ -12,4 +12,10 @@ class ParkingTest extends TestCase
         $parking = new Parking();
         $this->assertInstanceOf(Parking::class, $parking);
     }
+    
+    public function testLength()
+    {
+        $parking = new Parking();
+        $this->assertEquals(1, $parking->getLength());
+    }
 }
