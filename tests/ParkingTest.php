ce275b814fcdcce9b2c634fadb1843a139003158
Deimantas Abelskis <ddeimantass@gmail.com>
2018-06-13 19:38:29 +0300

    err1

---
+++ tests/ParkingTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Testing\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ParkingTest extends TestCase
+{
+    public function testParking()
+    {
+        $parking = new Parking();
+    }
+}
