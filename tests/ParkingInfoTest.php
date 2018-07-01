830d16975b063459be3c42b30e9803bf1d21fd64
Deimantas Abelskis <ddeimantass@gmail.com>
2018-07-01 12:44:02 +0300

    red

--- tests/ParkingInfoTest.php
+++ tests/ParkingInfoTest.php
@@ -3,6 +3,7 @@
 namespace Testing\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Testing\Model\Parking;
 use Testing\Provider\ParkingInfoProvider;
 
 class ParkingInfoTest extends TestCase
@@ -10,5 +11,7 @@ class ParkingInfoTest extends TestCase
     public function testParkingSpaceSuccess()
     {
         $parkingInfo = new ParkingInfoProvider();
+        $this->assertEquals(1, $parkingInfo->getSize(1, 1));
+        
     }
 }
