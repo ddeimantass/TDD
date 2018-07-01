e05b92d1a9cb4e78001869a48c0c6e9041aa6b2c
Deimantas Abelskis <ddeimantass@gmail.com>
2018-07-01 13:09:43 +0300

    blue

--- tests/ParkingInfoTest.php
+++ tests/ParkingInfoTest.php
@@ -9,11 +9,39 @@ use Testing\Provider\ParkingInfoProvider;
 
 class ParkingInfoTest extends TestCase
 {
-    public function testParkingSpace()
+    /**
+     * @dataProvider correctDataProvider
+     */
+    public function testParkingSpaceSuccess($cars, $parking, $exepcted)
     {
         $parkingInfo = new ParkingInfoProvider();
-        $parking = new Parking(1,2);
-        $car = new Car('', 1,1);
-        $this->assertEquals(1, $parkingInfo->getCarSpaces($parking, [$car]));
+        $this->assertEquals($exepcted, $parkingInfo->getCarSpaces($parking, $cars));
+    }
+    
+    /**
+     * @return array
+     */
+    public function correctDataProvider()
+    {
+        $cars =[
+            new Car('small',1,1),
+            new Car('suv',2,2),
+            new Car('mid',1,2),
+            new Car('bus',2,4),
+        ];
+        
+        $parkings =[
+            new Parking(1,1),
+            new Parking(2, 1),
+            new Parking(5, 3),
+            new Parking(7, 9),
+        ];
+        
+        return [
+            [$cars, $parkings[0], 0],
+            [$cars, $parkings[1], 1],
+            [$cars, $parkings[2], 3],
+            [$cars, $parkings[3], 4],
+        ];
     }
 }
