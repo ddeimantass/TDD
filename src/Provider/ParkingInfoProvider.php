c356359776c79f2a409786aaea1c2cb29b1c7171
Deimantas Abelskis <ddeimantass@gmail.com>
2018-07-01 12:44:53 +0300

    green

--- src/Provider/ParkingInfoProvider.php
+++ src/Provider/ParkingInfoProvider.php
@@ -4,5 +4,11 @@ namespace Testing\Provider;
 
 class ParkingInfoProvider
 {
-    
+    /**
+     * @return float|int
+     */
+    public function getSize($width, $length)
+    {
+        return $width * $length;
+    }
 }
