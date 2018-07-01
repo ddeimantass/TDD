3627841f94eacbf7eec19bf9f7946517489e5944
Deimantas Abelskis <ddeimantass@gmail.com>
2018-07-01 12:58:41 +0300

    green

--- src/Provider/ParkingInfoProvider.php
+++ src/Provider/ParkingInfoProvider.php
@@ -11,4 +11,28 @@ class ParkingInfoProvider
     {
         return $width * $length;
     }
+    
+    /**
+     * @param $parking
+     * @param $cars
+     * @return int
+     */
+    public function getCarSpaces($parking, $cars)
+    {
+        $parkingSize = $this->getSize($parking->getWidth(), $parking->getLength());
+        $CarsSize = [];
+        foreach($cars as $car){
+            $CarsSize[] = $this->getSize($car->getWidth(), $car->getLength());
+        }
+        $counter = 0;
+        foreach ($CarsSize as $carSize){
+            $parkingSize -= $carSize * 1.5;
+            if($parkingSize <= 0){
+                return $counter;
+            }
+            $counter++;
+        }
+        
+        return $counter;
+    }
 }
--- tests/ParkingInfoTest.php
+++ tests/ParkingInfoTest.php
@@ -3,15 +3,24 @@
 namespace Testing\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Testing\Model\Car;
 use Testing\Model\Parking;
 use Testing\Provider\ParkingInfoProvider;
 
 class ParkingInfoTest extends TestCase
 {
-    public function testParkingSpaceSuccess()
+    public function testSize()
     {
         $parkingInfo = new ParkingInfoProvider();
         $parking = new Parking(1,1);
         $this->assertEquals(1, $parkingInfo->getSize($parking->getWidth(), $parking->getLength()));
+    }
+    
+    public function testParkingSpace()
+    {
+        $parkingInfo = new ParkingInfoProvider();
+        $parking = new Parking(1,2);
+        $car = new Car('', 1,1);
+        $this->assertEquals(1, $parkingInfo->getCarSpaces($parking, [$car]));
     }
 }
