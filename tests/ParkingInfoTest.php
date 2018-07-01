de338396be30eaf102731d8695257a3a1eb6797d
Deimantas Abelskis <ddeimantass@gmail.com>
2018-07-01 12:37:15 +0300

    red;

---
+++ tests/ParkingInfoTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Testing\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ParkingInfoTest extends TestCase
+{
+    public function testParkingSpaceSuccess()
+    {
+        $parkingInfo = new ParkingInfoProvider();
+    }
+}
